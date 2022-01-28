705865f2ca5012bf6f61663735167a148351d2da
vidklopcic <klopcic.vid@gmail.com>
2022-01-28 09:08:31 +0100

    re-add _default_id due to migrations

--- proto_socket_django/api_models.py
+++ proto_socket_django/api_models.py
@@ -9,6 +9,10 @@ from django.db import models
 from django.conf import settings
 
 
+def _default_id():
+    return str(uuid.uuid1())
+
+
 class ApiModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     date_created = models.DateTimeField(auto_now_add=True)
