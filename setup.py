f185f82c8f314f95b2b0af8bbf7bbe32b18c44b5
vidklopcic <klopcic.vid@gmail.com>
2022-03-22 20:03:26 +0000

    fix name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
 setup(
-    name='proto-socket-django.py',
+    name='proto-socket-django',
     version='0.1',
     packages=['proto_socket_django'],
     description='A simple library that works with flutter_persistent_socket library.',
