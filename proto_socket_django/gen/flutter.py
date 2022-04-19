95ba08b7d5867992ad12dce3bcefebee2245eb08
vidklopcic <klopcic.vid@gmail.com>
2022-04-19 18:12:19 +0200

    flutter gen fix

--- proto_socket_django/gen/flutter.py
+++ proto_socket_django/gen/flutter.py
@@ -9,9 +9,16 @@ if not os.path.isfile('pubspec.yaml') or not os.path.isfile('fps_config.json'):
     print('Run the command from flutter project root, containing fps_config.json and pubspec.yaml.')
     sys.exit(1)
 
-proto_out = 'src/proto'
+proto_out = 'lib/proto'
 delete_existing(proto_out)
 proto_path, protos = get_protos(json.load(open('fps_config.json')), '-I')
-subprocess.run(f'pbjs {proto_path} -t static-module -w es6 -o {proto_out}/compiled.js {" ".join(protos)}', shell=True)
-subprocess.run(f'pbts -o {proto_out}/compiled.d.ts {proto_out}/compiled.js', shell=True)
+subprocess.run(f'protoc {proto_path} --dart_out={proto_out} {" ".join(protos)}', shell=True)
+subprocess.run(
+    "sed -i '' 's/sfiles.pb.dart/package:flutter_persistent_socket\/proto\/sfiles.pb.dart/g' ./lib/proto/*",
+    shell=True
+)
+subprocess.run(
+    "sed -i '' 's/uploader.pb.dart/package:flutter_persistent_socket\/proto\/uploader.pb.dart/g' ./lib/proto/*",
+    shell=True
+)
 generate(protos)
