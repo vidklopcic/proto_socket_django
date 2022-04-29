ae87b8919e065b75ddac5821299c62fa15271faf
vidklopcic <klopcic.vid@gmail.com>
2022-04-29 16:11:19 +0200

    hack to allow subclassing of generated proto messages

--- proto_socket_django/serializers.py
+++ proto_socket_django/serializers.py
@@ -5,5 +5,9 @@ from proto.messages import TxMessage, RxMessage
 class ProtoSerializer(abc.ABC):
     tx: TxMessage
 
+    def __new__(cls, *args, **kwargs):
+        cls.tx.proto()  # hack to set original vars() to current module (not really sure why it fails otherwise)
+        return super().__new__(cls)
+
     def msg(self):
         return self.tx(self)
