78ab6a612f45c1cf400bd84ba13915bb72bf1ce3
vidklopcic <klopcic.vid@gmail.com>
2022-06-01 15:53:31 +0200

    guard if tx is None

--- proto_socket_django/serializers.py
+++ proto_socket_django/serializers.py
@@ -6,7 +6,7 @@ class ProtoSerializer(abc.ABC):
     tx: TxMessage
 
     def __new__(cls, *args, **kwargs):
-        if cls.tx:
+        if hasattr(cls, 'tx'):
             cls.tx.proto()  # hack to set original vars() to current module (not really sure why it fails otherwise)
         return super().__new__(cls)
 
