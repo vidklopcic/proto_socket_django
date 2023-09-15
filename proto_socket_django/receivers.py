2e4ed5623d5591a93df73fa1fa4071acb80d2d0e
vidklopcic <klopcic.vid@gmail.com>
2023-09-15 09:35:50 +0200

    added authentication receiver

---
+++ proto_socket_django/receivers.py
@@ -0,0 +1,38 @@
+import proto_socket_django as psd
+import proto.messages as pb
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class AuthenticationReceiver(psd.FPSReceiver):
+    @psd.receive(auth=False)
+    def login(self, message: pb.RxLogin):
+        user = authenticate(username=message.proto.username, password=message.proto.password)
+
+        if user is None:
+            message = pb.TxLoginError(
+                pb.TxLoginError.proto(error_text='Invalid login details.', error_code='login-error')
+            )
+        else:
+            refresh = RefreshToken.for_user(user)
+            message = pb.TxLoginToken(pb.TxLoginToken.proto(
+                token=str(refresh.access_token),
+                refresh=str(refresh),
+            ))
+            self.consumer.token = message.proto.token
+        self.consumer.send_message(message)
+        self.consumer.authenticate()
+
+    @psd.receive(auth=False)
+    def refresh_token(self, message: pb.RxRefreshToken):
+        try:
+            refresh = RefreshToken(message.proto.refresh_token)
+            message = pb.TxLoginToken(pb.TxLoginToken.proto(
+                token=str(refresh.access_token),
+                refresh=str(refresh),
+            ))
+            self.consumer.token = message.proto.token
+        except Exception as e:
+            message = pb.TxRefreshTokenInvalid(pb.TxRefreshTokenInvalid.proto())
+        self.consumer.send_message(message)
+        self.consumer.authenticate()
