3ec05d4ff8c9f6e3ad98f746e084c51303652482
vidklopcic <klopcic.vid@gmail.com>
2023-09-22 15:58:04 +0200

    typos

--- proto_socket_django/__init__.py
+++ proto_socket_django/__init__.py
@@ -3,7 +3,6 @@ import sys
 if '-m' not in sys.argv:
     import proto.messages as pb
     from . import betterproto_patch
-    from . import api_models as api_models
     from .consumer import *
     from . import utils
     from .serializers import ProtoSerializer
