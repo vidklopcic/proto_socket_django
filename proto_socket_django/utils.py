141086bfdd0d31cd297d9325be76e9bffedd271c
vidklopcic <klopcic.vid@gmail.com>
2023-09-22 15:33:39 +0200

    typos

--- proto_socket_django/utils.py
+++ proto_socket_django/utils.py
@@ -1,10 +1,11 @@
 import zoneinfo
 from datetime import datetime
+from typing import Optional, Union
 
 from django.utils import timezone
 
 
-def to_timestamp(dt: timezone.datetime | datetime.date) -> int | None:
+def to_timestamp(dt: Union[timezone.datetime, datetime.date]) -> Optional[int]:
     if dt is None:
         return None
     if not hasattr(dt, 'timestamp'):
@@ -12,7 +13,7 @@ def to_timestamp(dt: timezone.datetime | datetime.date) -> int | None:
     return int(dt.timestamp() * 1000)
 
 
-def from_timestamp(ts) -> timezone.datetime | None:
+def from_timestamp(ts) -> Optional[timezone.datetime]:
     if ts is None:
         return None
     return timezone.datetime.fromtimestamp(ts / 1000, tz=zoneinfo.ZoneInfo('UTC'))
