29c58a4879177e1e55bdb7c17d10a65f331ac979
vidklopcic <klopcic.vid@gmail.com>
2024-01-04 09:43:23 +0100

    include common if true except for flutter

--- proto_socket_django/gen/react.py
+++ proto_socket_django/gen/react.py
@@ -12,6 +12,8 @@ def main():
         sys.exit(1)
 
     config = json.load(open('fps_config.json'))
+    if 'include_common' not in config:
+        config['include_common'] = True
     proto_out = config.get('out', 'src/proto')
     delete_existing(proto_out)
     proto_path, protos = get_protos(config, '-p')
